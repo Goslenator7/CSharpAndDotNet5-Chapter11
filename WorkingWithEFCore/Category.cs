6d0a7da96075129a4d82bad5433813edd4d76d6a
ryan <ryancumming7@gmail.com>
2022-05-23 15:57:23 +0100

    Added northwind, product and category classes

---
+++ WorkingWithEFCore/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Packt.Shared
+{
+	public class Category
+	{
+		
+	}
+}
---
+++ WorkingWithEFCore/Northwind.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace Packt.Shared  
+{
+	public class Northwind
+	{
+		
+	}
+}
+
