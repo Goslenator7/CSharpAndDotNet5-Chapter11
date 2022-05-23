0e57ec91e8d65fc50b22b9f69f1d46c845037b72
ryan <ryancumming7@gmail.com>
2022-05-23 16:58:52 +0100

    Added filter db query to main and call

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using static System.Console;
+using Packt.Shared;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace WorkingWithEFCore
 {
     class Program
     {
+
+        static void QueryingCategories()
+        {
+           using (var db = new Northwind())
+           {
+             WriteLine("Categories and how many products they have:");
+
+             //a query to get all categories and their related products
+             IQueryable<Category> cats = db.Categories.Include(c => c.Products);
+
+             foreach (Category c in cats)
+             {
+               WriteLine($"{c.CategoryName} has {c.Products.Count} products.");
+             }
+           }
+        }
+
+        static void FilteredIncludes()
+        {
+            using (var db = new Northwind())
+            {
+                Write("Enter a minimum for units in stock: ");
+                string unitsInStock = ReadLine();
+                int stock = int.Parse(unitsInStock);
+
+                IQueryable<Category> cats = db.Categories.Include(c => c.Products.Where(p => p.Stock >= stock));
+
+                foreach (Category c in cats)
+                {
+                    WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock");
+
+                    foreach (Product p in c.Products)
+                    {
+                        WriteLine($" {p.ProductName} has {p.Stock} units in stock.");
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            
+            //QueryingCategories();
+            FilteredIncludes();
         }
     }
 }
