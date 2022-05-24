f3cde7a45eb41ec1a1fde1eed187e480bf3f232a
ryan <ryancumming7@gmail.com>
2022-05-24 11:36:41 +0100

    Added QueryingProducts method to main, output query in filteredIncludes method in main

--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -8,6 +8,30 @@ namespace WorkingWithEFCore
 {
     class Program
     {
+        static void QueryingProducts()
+        {
+            using (var db = new Northwind())
+            {
+                WriteLine("Products that cost more than a price, highest cost first: ");
+                string input;
+                decimal price;
+                do
+                {
+                    Write("Enter a product price: ");
+                    input = ReadLine();
+                }
+                while (!decimal.TryParse(input, out price));
+
+                IQueryable<Product> prods = db.Products
+                    .Where(product => product.Cost > price)
+                    .OrderByDescending(product => product.Cost);
+
+                foreach (Product item in prods)
+                {
+                    WriteLine($"{item.ProductID}: {item.ProductName} costs {item.Cost:$#,##0.00} and has {item.Stock} in stock.");
+                }
+            }
+        }
 
         static void QueryingCategories()
         {
@@ -35,6 +59,9 @@ namespace WorkingWithEFCore
 
                 IQueryable<Category> cats = db.Categories.Include(c => c.Products.Where(p => p.Stock >= stock));
 
+                // output the SQL query created from the line above
+                WriteLine($"ToQueryString: {cats.ToQueryString()}");
+
                 foreach (Category c in cats)
                 {
                     WriteLine($"{c.CategoryName} has {c.Products.Count} products with a minimum of {stock} units in stock");
@@ -51,6 +78,7 @@ namespace WorkingWithEFCore
         {
             //QueryingCategories();
             FilteredIncludes();
+            //QueryingProducts();
         }
     }
 }
